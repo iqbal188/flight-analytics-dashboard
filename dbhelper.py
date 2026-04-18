d114db50fa8d6c046aefd578c0b2501ec4b5f5ca
Mohd Iqbal <iqbalmazhari.786@gmail.com>
2026-04-18 17:58:27 +0530

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -33,10 +33,12 @@ class Database:
 
     def fetch_all_flights(self,source,destination):
 
-        self.mycursor.execute("""
-        SELECT Airline, Route, Dep_Time, Duration, Price FROM flights
-        WHERE Source = '{}' AND Destination = '{}'
-        """.format(source,destination))
+        query = """
+        SELECT Airline, Route, Dep_Time, Duration, Price 
+        FROM flights
+        WHERE Source = %s AND Destination = %s
+        """
+        self.mycursor.execute(query, (source, destination))
 
         data = self.mycursor.fetchall()
 
@@ -71,7 +73,7 @@ class Database:
 							 UNION ALL
                              SELECT Destination FROM flights) t
         GROUP BY t.Source
-        ORDER BY COUNT(*)
+        ORDER BY COUNT(*) DESC
         """)
 
         data = self.mycursor.fetchall()
